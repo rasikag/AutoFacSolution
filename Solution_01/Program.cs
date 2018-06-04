6cda59e9ca8cbb6fc7b16ec3234e5ea29b7a27d0
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-06-04 23:04:21 +0800

    autofac init

---
+++ Solution_01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_01
+{
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+        public Engine(ILog log)
+        {
+            this.log = log;
+            id = new Random().Next();
+        }
+
+    }
+
+    public class Car
+    {
+        private Engine Engine;
+        private ILog log;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.log = log;
+            this.Engine = engine;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
